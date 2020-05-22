d66fd64fd63fd2893f40e535023f204b49aef6e2
RetroWraith <maxwell.w.potter@gmail.com>
2020-05-22 13:49:01 -0400

    Moved Recognize Team HEalth

--- index.py
+++ index.py
@@ -13,10 +13,24 @@ print("done waiting")
 ocr = MinecraftOCR.OCR(Image.open('D:\\Python\\AnniScraper\\ascii.png'))
 
 
+# Returns an array storing the health of each team formatted as [Blue, Green, Red, Yellow]
 def recognizeTeamHealth():
     img = ImageGrab.grab().crop(constant.TEAM_HEALTH_RECTANGLE)
     img.save('score.png', 'PNG')
-    return ocr.processImage(img)
+    scrapedText = ocr.processImage(img)
+
+    teamHealths = [0] * 4
+    switchCases = {
+        'B': 0,
+        'G': 1,
+        'R': 2,
+        'Y': 3
+    }
+    for team in scrapedText:
+        healthVal = int(team[len(team) - 2] + team[len(team) - 1])
+        teamHealths[switchCases[team[0]]] = healthVal
+        
+    return teamHealths
 
 
 print(recognizeTeamHealth())
